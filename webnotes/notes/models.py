1ae6f95541da3f1a978243b210d3001e403ecf8a
Nacolepsy <nacolepsy@ubuntu.ubuntu-domain>
2012-07-03 14:48:40 +0000

    My Heroku App

---
+++ webnotes/notes/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+class Notes(models.Model):
+	title= models.CharField(max_length=255)
+	author=models.CharField(max_length=255)
+	content = models.TextField()
+	def __unicode__(self):
+		return self.title
+
